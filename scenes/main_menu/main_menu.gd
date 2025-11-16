9e3884627d0896b307394195dd57915f70d55d7d
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2025-11-16 17:12:45 +0100

    updated menu

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -2,40 +2,23 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$VBoxContainer/Button2.visible = !OS.has_feature("web")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$FirstScreen/BoxContainer/Start/VBoxContainer/No.visible = !OS.has_feature("web")
 
 
 func _on_button_pressed() -> void:
 	#change scene
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
-	pass # Replace with function body.
 
 
 func _on_button_2_pressed() -> void:
-	$Label.text="a pos te quedas sin jugar"
-	$VBoxContainer/Button.visible=false
-	$VBoxContainer/Button2.visible=false
-	$VBoxContainer/Button3.visible=true
-	$VBoxContainer/Button4.visible=true
-	pass # Replace with function body.
+	$FirstScreen.hide()
+	$SecondScreen.show()
 
 
 func _on_button_3_pressed() -> void:
-	$Label.text="¿Me querrías si fuese un gusanito?"
-	$VBoxContainer/Button.visible=true
-	$VBoxContainer/Button2.visible=true
-	$VBoxContainer/Button3.visible=false
-	$VBoxContainer/Button4.visible=false
-	pass # Replace with function body.
+	$FirstScreen.show()
+	$SecondScreen.hide()
 
 
 func _on_button_4_pressed() -> void:
-	#exit game
 	get_tree().quit()
-	pass # Replace with function body.
