96484400eb95764df3cd9c2d65ea457421750279
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2025-11-16 12:29:24 +0100

    Update scenario

--- scenes/scenario/scenario_controller.gd
+++ scenes/scenario/scenario_controller.gd
@@ -4,6 +4,6 @@ extends CanvasLayer
 func _process(_delta: float) -> void:
 	$Sky.autoscroll.x = GameManager.movement / 64.0
 	$Middle.autoscroll.x = GameManager.movement / 16.0
-	$Font.autoscroll.x = GameManager.movement / 4.0
+	$Front.autoscroll.x = GameManager.movement / 4.0
 	$Ground.autoscroll.x = GameManager.movement
 	$Underground.autoscroll.x = GameManager.movement
